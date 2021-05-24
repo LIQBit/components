da797fc6306cc390f6800582a0c07903ee80ac7e
Leigh Bitossi <66171363+LIQBit@users.noreply.github.com>
2021-05-24 20:59:10 +0900

    Added Approval Cards

--- src/index.js
+++ src/index.js
@@ -7,22 +7,29 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-           <ApprovalCard />
-           <CommentDetail 
-             author="Sam" 
-             timeAgo="Today at 4:45pm" 
-             commentText="I like counting peanut shells." 
-             avatar={faker.image.avatar()}/> 
-           <CommentDetail 
-             author="Alex" 
-             timeAgo="Today at 6:05pm" 
-             commentText="I can't believe it's not butter!"
-             avatar={faker.image.avatar()}/> 
-           <CommentDetail 
-             author="Harry Plinkett" 
-             timeAgo="Yesterday at 2:25am" 
-             commentText="Would anyone like a pizza roll?"
-             avatar={faker.image.avatar()}/> 
+           <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45pm" 
+                    commentText="I like counting peanut shells." 
+                    avatar={faker.image.avatar()}/>
+           </ApprovalCard> 
+
+           <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 6:05pm" 
+                    commentText="I can't believe it's not butter!"
+                    avatar={faker.image.avatar()}/>
+           </ApprovalCard> 
+
+           <ApprovalCard>
+                <CommentDetail 
+                    author="Harry Plinkett" 
+                    timeAgo="Yesterday at 2:25am" 
+                    commentText="Would anyone like a pizza roll?"
+                    avatar={faker.image.avatar()}/> 
+           </ApprovalCard>
         </div>
     );
 };
